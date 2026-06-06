52c9a3f172336a1eecb08119ef88ddb53690d6c4
Md Asaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-06-06 23:20:01 +0600

    Add Components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
+
+
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white text-2xl">
-      Smart Notes App 🚀
-    </div>
-  );
+    <div>Home</div>
+  )
 }
