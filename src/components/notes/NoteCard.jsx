d7e4b783e7793fb2c8af649c87be500d40023fa7
Md Asaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-06-07 09:32:14 +0600

    Make a Note Form

--- src/components/notes/NoteCard.jsx
+++ src/components/notes/NoteCard.jsx
@@ -1,7 +1,11 @@
 
 
-export default function NoteCard() {
+export default function NoteCard({note, onDelete}) {
   return (
-    <div>NoteCard</div>
+    <div><div className="bg-gray-800 p-4 rounded-lg">
+      <h2 className="text-xl font-bold">{note.title}</h2>
+      <p className="text-gray-300 mt-2">{note.content}</p>
+    </div>
+    <button onClick={()=> onDelete(note.id)} className="mt-3 bg-red-600 px-3 py-1 rounded hover:bg-red-700">Delete</button></div>
   )
 }
--- src/components/notes/NoteForm.jsx
+++ src/components/notes/NoteForm.jsx
@@ -1,7 +1,39 @@
+import {useState} from "react"
 
+export default function NoteForm({onAdd}) {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
-export default function NoteForm() {
+  const handleSubmit = () => {
+    if (!title || !content) return;
+
+    onAdd(title, content);
+
+    setTitle("");
+    setContent("");
+  }
   return (
-    <div>NoteForm</div>
+    <div>
+      <input 
+      className="w-full p-2 bg-gray-700 rounded" 
+      placeholder="Note Title"
+      value={title}
+      onChange={(e)=> setTitle(e.target.value)}
+       />
+
+       <textarea 
+      className="w-full mt-3 mb-3 p-2 bg-gray-700 rounded" 
+      placeholder="Note Content"
+      value={content}
+      onChange={(e)=> setContent(e.target.value)}
+       />
+
+       <button 
+       onClick={handleSubmit}
+       className="bg-blue-600 px-4 py-2 rounded hover:bg-blue-700"
+       >
+        Add Note
+       </button>
+    </div>
   )
 }
