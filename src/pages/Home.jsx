af17faa82666d134cee98d3388aca62e8b2f6e36
Md Ashaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-06-08 11:50:27 +0600

    Apply UseEfffect and LocalStoreage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,46 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
 import NoteForm from "../components/notes/NoteForm";
 import NoteList from "../components/notes/NoteList";
 
 export default function Home() {
-    const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(() => {
+    const sevedNotes = localStorage.getItem("notes");
 
-    // Add Note
-
-    const addNote = (title, content) => {
-      const newNote ={
-        id: Date.now(),
-        title,
-        content,
-        createdAt: new Date(),
-      };
-      setNotes([newNote, ...notes]);
+    if (sevedNotes) {
+      return JSON.parse(sevedNotes);
     }
 
-    // Delete Note
-    const deleteNote = (id) =>{
-      const filtered = notes.filter((note)=> note.id !== id);
-      setNotes(filtered);
-    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  // Add Note
+
+  const addNote = (title, content) => {
+    const newNote = {
+      id: Date.now(),
+      title,
+      content,
+      createdAt: new Date(),
+    };
+    setNotes([newNote, ...notes]);
+  };
+
+  // Delete Note
+  const deleteNote = (id) => {
+    const filtered = notes.filter((note) => note.id !== id);
+    setNotes(filtered);
+  };
 
   return (
     <div className="min-h-screen bg-gray-900 text-white p-6">
       <h1 className=" text-3xl font-bold mb-6">Smart Note App</h1>
 
       <NoteForm onAdd={addNote} />
-      <NoteList notes={notes} onDelete={deleteNote}/>
+      <NoteList notes={notes} onDelete={deleteNote} />
     </div>
-  )
+  );
 }
