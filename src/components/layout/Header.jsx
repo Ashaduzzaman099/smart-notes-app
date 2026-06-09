2446990c798ab1e88f3ada6e2dffd3d2292bcb45
Md Ashaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-06-09 14:25:25 +0600

    Add Header Component

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <h1 className="text-3xl font-bold mb-6">Smart Notes App 🚀</h1>;
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Header from "../components/layout/Header";
 import NoteForm from "../components/notes/NoteForm";
 import NoteList from "../components/notes/NoteList";
 import useNotes from "../hooks/useNotes";
@@ -6,8 +7,7 @@ export default function Home() {
   const { notes, addNote, deleteNote } = useNotes();
   return (
     <div className="min-h-screen bg-gray-900 text-white p-6">
-      <h1 className=" text-3xl font-bold mb-6">Smart Note App</h1>
-
+      <Header />
       <NoteForm onAdd={addNote} />
       <NoteList notes={notes} onDelete={deleteNote} />
     </div>
