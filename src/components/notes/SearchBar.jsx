ed2e9978c194fc24483d8840894c7767815dd563
Md Asaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-06-10 09:02:41 +0600

    Search box added

---
+++ src/components/notes/SearchBar.jsx
@@ -0,0 +1,16 @@
+
+
+export default function SearchBar({
+    searchTerm,
+    setSearchTerm
+}) {
+    return (
+        <input 
+         type="text"
+         placeholder="Search Note..."
+         value={searchTerm}
+         onClick={(e)=>setSearchTerm(e.target.value)}
+         className="w-full p-3 mb-4 bg-gray-800 text-white rounded outline-none"
+        />
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
+import { useMemo, useState } from "react";
 import Header from "../components/layout/Header";
 import NoteForm from "../components/notes/NoteForm";
 import NoteList from "../components/notes/NoteList";
 import useNotes from "../hooks/useNotes";
 
+
+
 export default function Home() {
   const { notes, addNote, deleteNote } = useNotes();
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredNotes = useMemo(()=>{
+    return notes.filter((note)=>{
+      
+    })
+  })
+
+
   return (
     <div className="min-h-screen bg-gray-900 text-white p-6">
       <Header />
