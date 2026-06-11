fdb63bf6ad518ae14575fdbdb5369aa09d41441b
Md Ashaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-06-11 15:29:51 +0600

    Notes Context Add Now

--- src/context/NotesContext.jsx
+++ src/context/NotesContext.jsx
@@ -1,1 +1,26 @@
 import { createContext, useContext, useEffect, useReducer } from "react";
+
+const NotesContext = createContext();
+
+function notesReducer(state, action) {
+  switch (action.type) {
+    case "ADD_NOTE":
+      return [action.payload, ...state];
+
+    case "DELETE_NOTE":
+      return state.filter((note) => note.id !== action.payload);
+
+    case "UPDATE_NOTE":
+      return state.map((note) =>
+        note.id === action.payload.id
+          ? {
+              ...note,
+              ...action.payload.data,
+            }
+          : note,
+      );
+
+    default:
+      return state;
+  }
+}
