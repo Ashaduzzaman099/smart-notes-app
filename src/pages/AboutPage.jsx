eb669b208da28f04d13aa6e1c9f9baf23f780be1
Md Asaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-06-19 22:56:58 +0600

    Add 3 new and 1 componet

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,10 @@
+
+
+export default function AboutPage() {
+  return (
+    <div className="text-white p-4">
+        <h1 className="text-3xl font-bold mb-4">About Us</h1>
+        <p>Welcome to our note-taking app! We are dedicated to providing a simple and efficient way to manage your notes.</p>
+    </div>
+  )
+}
---
+++ src/pages/SettingsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SettingsPage() {
+  return (
+    <div className='text-white p-6'>
+        <h1 className='font-bold text-3xl'>Settings Page</h1>
+    </div>
+  )
+}
