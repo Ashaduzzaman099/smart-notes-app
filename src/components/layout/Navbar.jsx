4c67f6724c0019943ce172f51dbc106a11667eee
Md Ashaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-06-20 14:27:02 +0600

    Add navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,19 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="bg-gray-800 p-4">
+      <div className="flex gap-4">
+        <NavLink to="/" className="text-white">
+          Notes
+        </NavLink>
+        <NavLink to="/about" className="text-white">
+          About
+        </NavLink>
+        <NavLink to="/setting" className="text-white">
+          Setting
+        </NavLink>
+      </div>
+    </nav>
+  );
 }
