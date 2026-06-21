e3a638dc450612aa68f7d16de49cccb84f988e10
Md Ashaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-06-21 16:15:39 +0600

    Theme Context created

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router-dom";
+import { useTheme } from "../../context/ThemeContext";
 
 export default function Navbar() {
+  const { theme, toggleTheme } = useTheme();
   return (
     <nav className="bg-gray-800 p-4">
       <div className="flex gap-4">
@@ -13,6 +15,12 @@ export default function Navbar() {
         <NavLink to="/setting" className="text-white">
           Setting
         </NavLink>
+        <button
+          onClick={toggleTheme}
+          className="bg-gray-700 px-3 py-1 rounded text-white"
+        >
+          {theme === "dark" ? "☀️ Light" : "🌙 Dark"}
+        </button>
       </div>
     </nav>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import "./index.css";
 import { NotesProvider } from "./context/NotesContext";
+import { ThemeProvider } from "./context/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NotesProvider>
-      <App />
-    </NotesProvider>
+    <ThemeProvider>
+      <NotesProvider>
+        <App />
+      </NotesProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 );
